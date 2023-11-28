4bdce4fe17bc5f01a88dfdf44f0041517c982d5b
Kaanel <150800954+Kaanel@users.noreply.github.com>
2023-11-28 12:03:56 +0100

    Add files via upload

--- ese_tp2.ino
+++ ese_tp2.ino
@@ -1,73 +1,60 @@
 /* This file has been prepared for Doxygen automatic documentation generation.*/
 /*! \file *********************************************************************
-
-   \brief
-        Motor control implementation.
-
-        This file contains the full implementation of the motor control,
-        except the PID-controller.
-
-   \par Application note:
-        AVR447: Sinusoidal driving of three-phase permanent motor using
-        ATmega48/88/168
-
-   \par Documentation
-        For comprehensive code documentation, supported compilers, compiler
-        settings and supported devices see readme.html
-
-   \author
-        Atmel Corporation: http://www.atmel.com \n
-        Support email: avr@atmel.com
-
-   $Name: RELEASE_1_0 $
-   $Revision: 1.8 $
-   $RCSfile: main.c,v $
-   $Date: 2006/03/27 07:20:51 $  \n
+ *
+ * \brief
+ *      Transmition d'un code ASCII à l'aide d'une liaison série
+ *
+ * \par Note: A partir de la Datasheet ATmega328P
+ *
+ * \par Documentation: https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
+ *
+ * \author
+ *      Github: https://github.com/kaanel
+ *      Email: romainmarteaud@gmail.com
+ *
+ * $Name: Liaison série $
+ * $Revision: 1.0 $
+ * $RCSfile: main.c,v $
+ * $Date: 2023/11/27 11:03 $  \n
  ******************************************************************************/
-
-
-
-#define FOSC 16000000 // Clock Speed
+//#include <avr/io.h>
+#define FOSC 16000000  // Clock Speed
 #define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
-int flag =0;
+#define MYUBRR FOSC / 16 / BAUD - 1
 
+#define FRAMING_ERROR (1 << FE0)
+#define PARITY_ERROR (1 << UPE0)
+#define DATA_OVERRUN (1 << DOR0)
 
-int main(void)
-{
+int flag = 0;
+volatile char donnee;
+
+/*! \brief Fonction Main 
+ */
+int main(void) {
   USART_Init(MYUBRR);
+  USART_Transmit(USART_Receive() + 1);
   sei();
-  while (1)
-  {
-    if (flag =1)
-    {
-      USART_Transmit('A');
-      flag = 0;
-    }
-    _delay_ms(1000);
+  while (1) {
+    _delay_ms(1);
   }
 }
 
-
-ISR(USART0_RX_vect)
-{
-  flag = 1;
-}
-
-
-void USART_Init(unsigned int ubrr)
-{
+/*! \brief Fonction Initialisation de la communication
+ */
+void USART_Init(unsigned int ubrr) {
   /*Set baud rate */
   UBRR0H = (unsigned char)(ubrr >> 8);
   UBRR0L = (unsigned char)ubrr;
-  //Enable receiver and transmitter
-  UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+  //Enable receiver and transmitter * /
+  UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
   /* Set frame format: 8data, 2stop bit */
   UCSR0C = (1 << USBS0) | (3 << UCSZ00);
 }
 
-void USART_Transmit(unsigned char data)
-{
+/*! \brief Fonction Transmission de la communication
+ */
+void USART_Transmit(unsigned char data) {
   /* Wait for empty transmit buffer */
   while (!(UCSR0A & (1 << UDRE0)))
     ;
@@ -75,11 +62,46 @@ void USART_Transmit(unsigned char data)
   UDR0 = data;
 }
 
-unsigned char USART_Receive(void)
-{
+/*! \brief Fonction Reception de la communication
+ */
+unsigned char USART_Receive(void) {
   /* Wait for data to be received */
   while (!(UCSR0A & (1 << RXC0)))
     ;
-  /* Get and return received data from buffer */
-  return UDR0;
+  char status = UCSR0A;
+  if ((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN)) == 0) {
+    /* Get and return received data from buffer */
+    return UDR0;
+  } else {
+    UDR0;
+    USART_putsln("error");
+  }
+}
+
+/*! \brief https://www.nongnu.org/avr-libc/user-manual/group__avr__interrupts.html
+ */
+ISR(USART_RX_vect) {
+  char status = UCSR0A;
+  char byteReceive = UDR0;
+  if ((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN)) == 0) {
+    USART_Transmit(byteReceive);
+  } else {
+    USART_putsln("error");
+  }
+}
+
+/*! \brief Fonction transmission d'une chaine de caractère
+ */
+void USART_puts(unsigned char *str) {
+  do {
+    USART_Transmit(*str);
+  } while (*++str != 0);
+}
+
+/*! \brief Fonction sauter une ligne
+ */
+void USART_putsln(unsigned char *str) {
+  USART_puts(str);
+  USART_Transmit(10);
+  USART_Transmit(13);
 }
